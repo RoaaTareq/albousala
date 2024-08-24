9ab2f7596daa27ea992f62bfb8c010b39168fc1a
roaaabushaqrah <roaa.abushaqrah@gmail.com>
2024-08-24 13:37:13 +0300

    add new desgin

--- resources/views/survay/thankyou.blade.php
+++ resources/views/survay/thankyou.blade.php
@@ -7,46 +7,49 @@
 
     <!-- Styles -->
     <link rel="stylesheet" href="{{ asset('css/style.css') }}">
+    <link rel="stylesheet" href="{{ asset('css/style.css') }}">
+
     <link href="{{ asset('css/app.css') }}" rel="stylesheet">
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-QWTKZyjpPEjISv5WaRU9OFeRpok6YctnYmDr5pNlyT2bRjXh0JMhjY6hW+ALEwIH" crossorigin="anonymous">
+    
 </head>
 <style>
-    body{
-        padding: 120px;
-    }
     ul{
         list-style-type: none;
-        text-align: center;
+        text-align:center;
     }
-    .cont{
-        margin: 12px ;
-        justify-content:center;
-        flex-direction:column
-    }
-    .con2{
-        border: 4px solid orange;
-        border-radius: 12px;
-        margin: 12px 0;
-    }
-</style>
+    </style>
 <body>
-    <h1 class='text-center'>شكرا للمشاركة</h1>
+    
+    <h1 class='text-center'>شكرا    للمشاركة</h1>
+
+    <!-- @if(session('success'))
+        <p class='text-center'>{{ session('success') }}</p>
+    @endif -->
 
     @if(session('total_score') !== null)
-        <!-- Optional: Show total score if needed -->
+        <!-- <p class='text-center'> نتيجتك:  {{ session('total_score') }} % </p> -->
+        <!-- <ul>
+            <li>مجموعك: {{ $differencess }}</li>
+        </ul> -->
     @endif
 
-    <h2 class='text-center'> أعلى ثلاثة أحزاب تقارب  :</h2>
-    <div class='text-center d-flex cont'>
-        @foreach($parties->sortByDesc('total_score')->take(3) as $party)
-        <div class="con2">
-            <p>{{ $party->name_parties }}</p>
+    <h2 class='text-center'>تقارب الاحزاب:</h2>
+    <ul class='text-center'>
+        @foreach($parties as $party)
+            <!-- <li>{{ $party->name_parties }}: {{ $party->total_score }}</li> -->
+            <li>{{ $party->name_parties }}</li>
             @php
                 $difference = abs(100 - $party->total_score);
+                $differenceFrom100 = 100 - $party->total_score;
+                $differenceFrom1001 = abs($differenceFrom100 - $differencess);
+                $x=abs($differenceFrom1001-100)
             @endphp
-            <p>نسبة تقاربك مع الحزب: {{ $difference }} %</p>
-        </div>
+            <!-- <li>Difference from Party Score: {{ $difference }}</li> -->
+            <!-- <li>Difference from 100: {{ $differenceFrom100 }}</li> -->
+            <!-- <li>تففف {{ $differenceFrom1001 }}</li> -->
+            <li> نسبة تقاربك مع الحزب{{ $x }} %</li>
         @endforeach
-    </div>
+    </ul>
 </body>
 </html>
