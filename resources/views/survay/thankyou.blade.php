34d07551069e5167231f79fdd368a6558c9bf487
roaaabushaqrah <roaa.abushaqrah@gmail.com>
2024-08-31 23:16:43 +0300

    first version222

--- resources/views/survay/thankyou.blade.php
+++ resources/views/survay/thankyou.blade.php
@@ -5,6 +5,11 @@
     <link rel="stylesheet" href="{{ asset('css/app.css') }}">
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/css/bootstrap.min.css" rel="stylesheet">
 </head>
+<style>
+    ul{
+        list-style-type:none;
+    }
+    </style>
 <body>
     <h1 class="text-center">شكرا للمشاركة</h1>
 
