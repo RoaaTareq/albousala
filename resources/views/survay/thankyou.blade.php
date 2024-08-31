304a0c315d119795d4682dc49dfebe2491e0cdc0
Roaa  Tareq Abu-shaqrah <105088305+RoaaTareq@users.noreply.github.com>
2024-08-31 19:06:33 +0300

    Update thankyou.blade.php

--- resources/views/survay/thankyou.blade.php
+++ resources/views/survay/thankyou.blade.php
@@ -1,22 +1,16 @@
 <!DOCTYPE html>
 <html dir='rtl'>
 <head>
-    <title>Thank You</title>
+    <title>Results</title>
     <link rel="stylesheet" href="{{ asset('css/app.css') }}">
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/css/bootstrap.min.css" rel="stylesheet">
 </head>
 <body>
-    <h1 class="text-center">شكرا للمشاركة</h1>
-
-    <h2 class="text-center">تقارب الاحزاب:</h2>
+    <h2 class="text-center">نتائج الاستطلاع</h2>
     <ul class="text-center">
-        @foreach($parties as $index => $party)
+        @foreach($parties as $party)
             <li>{{ $party->name_parties }}</li>
-            @if($total_score == 0)
-                <li>نسبة تقاربك مع الحزب: -100 %</li>
-            @else
-                <li>نسبة تقاربك مع الحزب: {{ abs($differences[$index]) }} %</li>
-            @endif
+            <li>نسبة تقاربك مع الحزب: {{ $party->difference }} %</li>
         @endforeach
     </ul>
 </body>
